2a7c1e23b20c997eb6e720c41f45509883439fda
Mariya <ma227ji@student.lnu.se>
2025-12-07 02:51:11 +0100

    "RED" removed reapeted tests + new tests

--- java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
+++ java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
@@ -2,6 +2,8 @@ package com.lab;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,20 +12,18 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Test class for Password implementations.
- * 
+ *
  * To test different buggy versions, simply uncomment the corresponding
  * getPassword() method and comment out the others.
- * 
- * Available implementations:
- * - Password: Correct implementation
- * - BugDoesNotTrim: Does not trim whitespace
- * - BugToShortPassword: Allows passwords shorter than 12 characters
- * - BugVeryShort: Allows way to short passwords
- * - BugWrongExceptionMessage: Wrong exception message for short passwords
- * - BugMissingPasswordLengthCheck: Does not throw exception for short passwords
- * - BugMissingNumberCheck: Does not throw exception if password lacks a number
- * - BugIsPasswordSameAlwaysTrue: isPasswordSame always returns true
- * - BugWrongHashingAlgorithm: Wrong hashing algorithm
+ *
+ * Available implementations: - Password: Correct implementation -
+ * BugDoesNotTrim: Does not trim whitespace - BugToShortPassword: Allows
+ * passwords shorter than 12 characters - BugVeryShort: Allows way to short
+ * passwords - BugWrongExceptionMessage: Wrong exception message for short
+ * passwords - BugMissingPasswordLengthCheck: Does not throw exception for short
+ * passwords - BugMissingNumberCheck: Does not throw exception if password lacks
+ * a number - BugIsPasswordSameAlwaysTrue: isPasswordSame always returns true -
+ * BugWrongHashingAlgorithm: Wrong hashing algorithm
  */
 public class PasswordTest {
 
@@ -33,23 +33,16 @@ public class PasswordTest {
         // return (IPassword) new BugToShortPassword(s);
         // return (IPassword) new BugToShortPassword(s);
         // return (IPassword) new BugVeryShort(s);
-        return (IPassword) new BugWrongExceptionMessage(s);
+        // return (IPassword) new BugWrongExceptionMessage(s);
         // return (IPassword) new BugMissingPasswordLengthCheck(s);
         // return (IPassword) new BugMissingNumberCheck(s);
         // return (IPassword) new BugIsPasswordSameAlwaysTrue(s);
-        // return (IPassword) new BugWrongHashingAlgorithm(s);
+        return (IPassword) new BugWrongHashingAlgorithm(s);
     }
 
     @Test
     public void shouldAlwaysPass() throws Exception {
         assertTrue(true);
-    }
-
-    // pass if not empty
-    @Test
-    public void NotEmpty() throws Exception {
-        IPassword NotEmptyPassword = getPassword("NotEmpty123!");
-        assertTrue(NotEmptyPassword != null);
     }
 
     // test trimming 
@@ -63,8 +56,15 @@ public class PasswordTest {
     // test too short password
     @Test
     public void TestTooShortPassword() throws Exception {
-        assertThrows(Exception.class, () -> {getPassword("abcnbhtgkj");});
+        assertThrows(Exception.class, () -> getPassword("abcrtu87123"),
+                "Should throw exception for password shorter than 12 characters");
 
+    }
+
+    // password with numbers
+    @Test
+    public void testPasswordWithNumbers() {
+        assertDoesNotThrow(() -> getPassword("Correctt123!"));
     }
 
 
